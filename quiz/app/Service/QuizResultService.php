e0f9b07968f795b36ed0cc27cb664f2131d7a7bf
Stanislav Dmitriev <dmistas@gmail.com>
2022-01-16 17:22:46 +0500

    implemented QuizResultService

--- quiz/app/Service/QuizResultService.php
+++ quiz/app/Service/QuizResultService.php
@@ -9,6 +9,7 @@ use App\DTO\AnswersDTO;
 use App\DTO\AnswerDTO;
 
 use Exception;
+use phpDocumentor\Reflection\Types\Boolean;
 
 class QuizResultService
 {
@@ -23,6 +24,38 @@ class QuizResultService
 
     public function getResult(): float
     {
-        // your code here
+        $countQuestions = 0;
+        $countRightAnswers = 0;
+        foreach ($this->quiz->getQuestions() as $question) {
+            $countQuestions += 1;
+            if ($this->checkAnswer($question)) {
+                $countRightAnswers += 1;
+            }
+        }
+        return $countRightAnswers / $countQuestions;
+    }
+
+    private function checkAnswer(QuestionDTO $question): bool
+    {
+        $result = false;
+        foreach ($this->answers->getAnswers() as $answer) {
+            if ($question->getUUID() !== $answer->getQuestionUUID()) {
+                continue;
+            }
+            $correctAnswers = collect($question->getChoices())->filter(function (ChoiceDTO $choice) {
+                return $choice->isCorrect();
+            })->map(function ($choice) {
+                return $choice->getUUID();
+            });
+            if ($correctAnswers->count() !== count($answer->getСhoices())) {
+                break;
+            }
+            if (!$correctAnswers->diff($answer->getСhoices())->count()) {
+                break;
+            }
+            $result = true;
+            break;
+        }
+        return $result;
     }
 }
